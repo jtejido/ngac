25cb1f69ac270fedc832130f1452a89adc16cb34
unknown <jericko@learnt.io>
2021-03-02 01:11:47 +0800

    used iterative deepening search on reviewauditor and decider

--- pkg/pip/graph/searcher.go
+++ pkg/pip/graph/searcher.go
@@ -22,7 +22,7 @@ type Visitor func(*Node) error
 
 type BFS struct {
 	graph Graph
-	seen  set.Set
+	seen  map[string]struct{}
 }
 
 func NewBFS(gr Graph) *BFS {
@@ -33,9 +33,9 @@ func (bfs *BFS) Traverse(start *Node, propagator Propagator, visitor Visitor, di
 	// set up a queue to ensure FIFO
 	queue := list.New()
 	// set up a set to ensure nodes are only visited once
-	bfs.seen = set.NewSet()
+	bfs.seen = make(map[string]struct{})
 	queue.PushBack(start)
-	bfs.seen.Add(start.Name)
+	bfs.seen[start.Name] = struct{}{}
 
 	for queue.Len() > 0 {
 		qval := queue.Front()
@@ -52,14 +52,14 @@ func (bfs *BFS) Traverse(start *Node, propagator Propagator, visitor Visitor, di
 			}
 
 			// if this node has already been seen, we don't need to se it again
-			if bfs.seen.Contains(n.Name) {
+			if _, ok := bfs.seen[n.Name]; ok {
 				continue
 			}
 
 			// add the node to the queue and the seen set
 			queue.PushBack(n)
-			bfs.seen.Add(n.Name)
 
+			bfs.seen[n.Name] = struct{}{}
 			// propagate from the nextLevel to the current node
 			if err := propagator(node, n); err != nil {
 				return err
@@ -82,20 +82,20 @@ func (bfs *BFS) nextLevel(node string, direction Direction) set.Set {
 
 type DFS struct {
 	graph   Graph
-	visited set.Set
+	visited map[string]struct{}
 }
 
 func NewDFS(gr Graph) *DFS {
-	return &DFS{gr, set.NewSet()}
+	return &DFS{gr, make(map[string]struct{})}
 }
 
 func (dfs *DFS) Traverse(start *Node, propagator Propagator, visitor Visitor, direction Direction) error {
-	if dfs.visited.Contains(start.Name) {
+	if _, ok := dfs.visited[start.Name]; ok {
 		return nil
 	}
 
 	// mark the node as visited
-	dfs.visited.Add(start.Name)
+	dfs.visited[start.Name] = struct{}{}
 
 	nextLevel := dfs.nextLevel(start.Name, direction)
 	for s := range nextLevel.Iter() {
@@ -123,4 +123,65 @@ func (dfs *DFS) nextLevel(node string, direction Direction) set.Set {
 	}
 
 	return dfs.graph.Children(node)
+}
+
+type IDS struct {
+	graph   Graph
+	limit   int
+	visited map[string]struct{}
+}
+
+func NewIDS(gr Graph, limit int) *IDS {
+	return &IDS{gr, limit, make(map[string]struct{})}
+}
+
+func (ids *IDS) Traverse(start *Node, propagator Propagator, visitor Visitor, direction Direction) error {
+	var depth int
+	node := start
+	var err error
+	for depth < ids.limit && node != nil {
+		node, err = ids.dls(node, propagator, visitor, direction, depth)
+		if err != nil {
+			return err
+		}
+
+		depth++
+	}
+
+	// after processing the parents, visit the start node
+	return visitor(start)
+}
+
+func (ids *IDS) dls(n *Node, propagator Propagator, visitor Visitor, direction Direction, depth int) (*Node, error) {
+	if _, ok := ids.visited[n.Name]; ok {
+		return nil, nil
+	}
+	// mark the node as visited
+	ids.visited[n.Name] = struct{}{}
+
+	nextLevel := ids.nextLevel(n.Name, direction)
+
+	for s := range nextLevel.Iter() {
+		node, err := ids.graph.Node(s.(string))
+		if err != nil {
+			return nil, err
+		}
+
+		// traverse from the node
+		ids.dls(node, propagator, visitor, direction, depth-1)
+
+		if err := propagator(node, n); err != nil {
+			return nil, err
+		}
+	}
+
+	return nil, visitor(n)
+}
+
+func (ids *IDS) nextLevel(node string, direction Direction) set.Set {
+	if direction == PARENTS {
+		return ids.graph.Parents(node)
+	}
+
+	return ids.graph.Children(node)
 }
